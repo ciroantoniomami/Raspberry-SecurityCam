1559e3989c886e4eea74931f04a0316189893eee
ciroantoniomami <mamiciroantonio@gmail.com>
2021-07-11 15:41:27 +0200

    update

--- yolo/yolo.py
+++ yolo/yolo.py
@@ -2,8 +2,8 @@ import sys
 import os 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from backbone import backbone
-from CSP import ConvBlock
+from yolo.backbone import backbone
+from yolo.CSP import ConvBlock
 
 import torch.nn as nn
 import torch
