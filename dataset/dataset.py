36bab7b22e9625ee01918feca8c93fcec6260d71
ciroantoniomami <mamiciroantonio@gmail.com>
2021-07-12 17:57:34 +0200

    coco

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -113,16 +113,16 @@ def get_data(train_csv_path):
         A.PadIfNeeded(
             min_height=IMAGE_SIZE, min_width=IMAGE_SIZE, border_mode=0
         ),
-        #A.OneOf(
-        #    [
-        #        A.ShiftScaleRotate( 
-        #            rotate_limit=20, p=0.5, border_mode=0
-        #        ),
-        #        A.Affine(shear=15, p=0.5),
-        #    ],
-        #    p=1.0,
-        #),
-        #A.HorizontalFlip(p=0.5),
+        A.OneOf(
+            [
+                A.ShiftScaleRotate( 
+                    rotate_limit=20, p=0.5, border_mode=0
+                ),
+                A.Affine(shear=15, p=0.5),
+            ],
+            p=1.0,
+        ),
+        A.HorizontalFlip(p=0.5),
         A.Normalize(mean=[0., 0., 0.], std=[1., 1., 1.], max_pixel_value=255,),
         A.Downscale (scale_min=0.25, scale_max=0.25, interpolation=0, always_apply=True, p=1),
         A.MotionBlur(p=1),
